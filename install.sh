3b93bf2f0e8acebb30b895235c8fb08e7b74825d
Eli Array Minkoff <eli@earrayminkoff.tech>
2022-03-19 13:53:33 -0400

    Save install path for uninstall

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 INSTALL_PATH="${MDPDF_INSTALL_PATH:-'~/.local/bin/mdpdf'}"
 CSS_PATH="${MDPDF_CSS_PATH:-'~/.local/share/mdpdf/custom.css'}"
-
 mkdir -p "$(dirname "$INSTALL_PATH")" "$(dirname "$CSS_PATH")"
 
 cd "$(dirname $0)"
@@ -12,3 +11,8 @@ cp custom.css "$CSS_PATH"
 envsubst < mdpdf > "$INSTALL_PATH"
 
 chmod +x "$INSTALL_PATH"
+
+cat <<EOF > install.env
+export INSTALL_PATH=$INSTALL_PATH
+export CSS_PATH=$CSS_PATH
+EOF
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-INSTALL_PATH="${MDPDF_INSTALL_PATH:-'~/.local/bin/mdpdf'}"
-CSS_PATH="${MDPDF_CSS_PATH:-'~/.local/share/mdpdf/custom.css'}"
+envfile="$(dirname "$(realpath "$0")")/install.env"
+
+if [ -f "$envfile" ]; then
+    . "$envfile"
+else
+    INSTALL_PATH="${MDPDF_INSTALL_PATH:-'~/.local/bin/mdpdf'}"
+    CSS_PATH="${MDPDF_CSS_PATH:-'~/.local/share/mdpdf/custom.css'}"
+fi
 
 rm "$INSTALL_PATH"
 rm "$CSS_PATH"
